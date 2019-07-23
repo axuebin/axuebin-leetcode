a97097e67106d36961188a7ddbb4baf416a3f3ce
axuebin <xbmail2013@163.com>
2019-07-23 13:03:14 +0800

    :memo: add 111, 112

---
+++ easy/112.路径总和.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=112 lang=javascript
+ *
+ * [112] 路径总和
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} sum
+ * @return {boolean}
+ */
+var hasPathSum = function(root, sum) {
+    if (!root) return false;
+    if (!root.left && !root.right && root.val === sum) {
+        return true;
+    }
+    return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+};
+
